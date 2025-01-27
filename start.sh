4f8c296522384208536fb08d147b243cda344699
hex0mega <vlsu8752@colorado.edu>
2025-01-26 19:23:05 -0700

    changing worker class again

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 # gunicorn -w 4 -k uvicorn.workers.UvicornWorker app:app
-gunicorn -k uvicorn.workers.UvicornWorker main:app
+gunicorn --worker-class uvicorn.workers.UvicornWorker --timeout 600 --access-logfile '-' --error-logfile '-' app:app
