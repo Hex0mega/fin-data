bdee860c16c128da3b63fb2cbbfbbb200fb535c1
hex0mega <vlsu8752@colorado.edu>
2025-01-28 12:55:43 -0700

    Removing azure connection logic until further notice

--- app.py
+++ app.py
@@ -19,25 +19,25 @@ def get_investments(ticker: str):
         return {"price":price}
     
 #connect to Azure SQL db
-connection_string = os.getenv("AZURE_SQL_CONNECTIONSTRING")
-print(connection_string)
+# connection_string = os.getenv("AZURE_SQL_CONNECTIONSTRING")
+# print(connection_string)
 
-def get_conn():
-    credential = identity.DefaultAzureCredential(exclude_interactive_browser_credential=False)
-    token_bytes = credential.get_token("https://database.windows.net/.default").token.encode("UTF-16-LE")
-    token_struct = struct.pack(f'<I{len(token_bytes)}s', len(token_bytes), token_bytes)
-    SQL_COPT_SS_ACCESS_TOKEN = 1256  # This connection option is defined by microsoft in msodbcsql.h
-    conn = pyodbc.connect(connection_string, attrs_before={SQL_COPT_SS_ACCESS_TOKEN: token_struct})
-    return conn
+# def get_conn():
+#     credential = identity.DefaultAzureCredential(exclude_interactive_browser_credential=False)
+#     token_bytes = credential.get_token("https://database.windows.net/.default").token.encode("UTF-16-LE")
+#     token_struct = struct.pack(f'<I{len(token_bytes)}s', len(token_bytes), token_bytes)
+#     SQL_COPT_SS_ACCESS_TOKEN = 1256  # This connection option is defined by microsoft in msodbcsql.h
+#     conn = pyodbc.connect(connection_string, attrs_before={SQL_COPT_SS_ACCESS_TOKEN: token_struct})
+#     return conn
 
-@app.put("/investment/{ticker}")
-def put_investments(ticker: str):
-    with get_conn() as conn:
-        cursor = conn.cursor()
-        dat = yf.Ticker(ticker)
-        price = dat.analyst_price_targets['current']
-        query = f"UPDATE Investments SET LastPrice=? WHERE Ticker=?"
-        params = (price, ticker)
-        cursor.execute(query, params)
-        conn.commit()
-        return price
+# @app.put("/investment/{ticker}")
+# def put_investments(ticker: str):
+#     with get_conn() as conn:
+#         cursor = conn.cursor()
+#         dat = yf.Ticker(ticker)
+#         price = dat.analyst_price_targets['current']
+#         query = f"UPDATE Investments SET LastPrice=? WHERE Ticker=?"
+#         params = (price, ticker)
+#         cursor.execute(query, params)
+#         conn.commit()
+#         return price
